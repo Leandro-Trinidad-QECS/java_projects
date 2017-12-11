ba1d048711eed76498fb1c5769f85e95e57154a7
Leandro <l.trinidad@share.epsb.ca>
2017-12-10 19:08:06 -0700

    updated password.java

--- password/password.java
+++ password/password.java
@@ -1,3 +1,4 @@
+import java.io.*;
 public class password {
 	public static void main(String[] args)
 	throws Exception
@@ -15,12 +16,22 @@ public class password {
 		}
 
 		// modes can be store or checks
-		System.out.println(args[0]);
 		if (args[0].equals("-s") || args[0].equals("-store")) {
 			//store username and password
 
+			if(writeToFile(args[1],args[2])) {
+				System.out.println("Account added to database");
+			} else {
+				System.out.println("Account already in the database");
+			}
+
 		} else if (args[0].equals("-c") || args[0].equals("-check")) {
 			//checks the password if in the database
+			if(checkUserPass(args[1],args[2])) {
+				System.out.println("Aacount is in the database");
+			} else {
+				System.out.println("Account not in the database");
+			}
 
 			//TODO: database is a textfile that has all the username and passwords
 		} else {
@@ -32,5 +43,33 @@ public class password {
 
 
 	}
+
+
+public static boolean writeToFile(String username, String password) throws Exception {
+		if (!checkUserPass(username,password)) {
+			BufferedWriter out = new BufferedWriter(new FileWriter("pass.txt",true));
+			out.write(username + ":" + password);
+			out.newLine();
+			out.close();
+			return true;
+		}
+		return false;
+
+}
+public static boolean checkUserPass(String user, String pass) throws Exception {
+	BufferedReader br = null;
+	br = new BufferedReader(new FileReader("pass.txt"));
+            String line;
+						String[]  userPass;
+            while ((line = br.readLine()) != null) {
+							userPass = line.split(":");
+							if (user.equals(userPass[0]) && pass.equals(userPass[1])) {
+								return true;
+							}
+            }
+						return false;
+}
+
+
 
 }
