5f161a48f1df3d9cc4572cf406c0851e6faadc0d
Lean <l.trinidad@share.epsb.ca>
2017-12-19 12:10:48 -0700

    added files

--- try catch/numbersTryCatch/numbersTryCatch.java
+++ try catch/numbersTryCatch/numbersTryCatch.java
@@ -6,42 +6,77 @@ import java.text.DecimalFormat;
 
 public class numbersTryCatch {
 
-  public static void main(String[] args) throws Exception {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Money Amount:");
-    while (!catchArgs(input.next())) {
-      System.out.println("Error");
-      System.out.print("Money Amount:");
-    }
+public static Map<String, Double> market = new HashMap<String, Double>();
+
+public static void main(String[] args) throws Exception {
+        //vars
+        String money_amount;
+        Scanner input = new Scanner(System.in);
+
+        //try catch statement
+        int i = 0;
+        while (true) {
+                System.out.print("How much money do you have ['50.65']: ");
+                money_amount = input.next();
+                if (catchArgs(money_amount)) {
+                        i = 0;
+                        break;
+                } else if (i >= 2) {
+                        System.out.println("Maybe you can type later");
+                        System.exit(0);
+                } else {
+                        System.out.println("Try Again");
+                        i++;
+                }
+        }
 
 
 
-  }
 
-  public static String[] catchArgs(Object money) throws Exception {
+}
 
-    // checks if the money is an interger or not
-    try {
-      Double.parseDouble(String.valueOf(money));
-      return true;
-    } catch (Exception e) {
-      return false;
-    }
-  }
-  public static Double formatMoney(Object money) {
-    //formats the money to two decimal places
+public static Boolean catchArgs(Object money) throws Exception {
+        // checks if the money is an interger or not
+        try {
+                Double.parseDouble(String.valueOf(money));
+                return true;
+        } catch (Exception e) {
+                return false;
+        }
+}
 
-    //turns object to string
-    String money_string = money.toString();
-    //turns string to double
-    double money_double = Double.parseDouble(money_string);
 
-    //decimal format
-    DecimalFormat df = new DecimalFormat("#.00");
+public static String formatMoney(Object money, boolean print) {
+        //formats the money to two decimal places
+        // cna print
 
-    //formats the double & returns it
-    String return_string = df.format(money_double);
-    double return_double = Double.parseDouble(return_string);
-    return return_double;
-  }
+        DecimalFormat df;
+        //turns object to string
+        String money_string = money.toString();
+        //turns string to double
+        double money_double = Double.parseDouble(money_string);
+
+        //decimal format
+        if (print) {
+                df = new DecimalFormat("$#.00");
+        } else {
+                df = new DecimalFormat("#.00");
+        }
+        //formats the double & returns it
+        String return_string = df.format(money_double);
+        // double return_double = Double.parseDouble(return_string);
+        return return_string;
+}
+
+public static boolean isInt(Object num) throws Exception {
+        //checks if object is an interger if its a string or double sends false
+        try {
+                int i = Integer.parseInt((String) num);
+                return true;
+        }
+        catch (Exception e) {
+                return false;
+        }
+
+}
 }
